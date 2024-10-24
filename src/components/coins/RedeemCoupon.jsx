efa895b8c4a8b2dee0dde94120a619c989934f3d
Titus Njema <njema3@gmail.com>
2024-10-24 14:26:21 +0300

    first commit

---
+++ src/components/coins/RedeemCoupon.jsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+const RedeemCoupon = () => {
+  return (
+    <div className='redeem'>
+      <div className="top">
+        <h3>Redeem Coupon</h3>        
+      </div>
+      <div className="bottom">
+        <input type="text"/>          
+      </div>
+      <div className="info">
+        <button>REDEEM COUPON</button>                    
+      </div>      
+    </div>
+  )
+}
+
+export default RedeemCoupon;
