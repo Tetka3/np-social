acd4056b94ea849e75db09be03eddab99f7f8cee
Titus Njema <njema3@gmail.com>
2024-10-24 13:40:10 +0300

    first commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,86 +1,86 @@
-// import "./home.css";
-// import write from "../assets/write.png";
-// import { useNavigate } from "react-router-dom";
-// import { useState } from "react";
+import "./home.css";
+import write from "../assets/write.png";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-// const Register = () => {
+const Register = () => {
 
-//   const [registerData, setRegisterData] = useState({
-//     fullname: "",
-//     gender: "",
-//     username: "",
-//     password: "",
-//     email: "",
-//     repeatPassword: ""
-//   })
-//   const navigate = useNavigate();
+  const [registerData, setRegisterData] = useState({
+    fullname: "",
+    gender: "",
+    username: "",
+    password: "",
+    email: "",
+    repeatPassword: ""
+  })
+  const navigate = useNavigate();
 
-//   const handleChange = (e) => {
-//     setFormData(prevData => {
-//       return {...prevData, [e.target.name]: e.target.value}
-//     })
-//   }
+  const handleChange = (e) => {
+    setFormData(prevData => {
+      return {...prevData, [e.target.name]: e.target.value}
+    })
+  }
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//   }
-//   return (
-//     <div className="register">
-//       <div className="imaged">
-//         <img src={write} />
-//       </div>
-//       <form className="registerForm" onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           placeholder="Fullname..."
-//           name = "fullname"
-//           value = {registerData.fullname}
-//           onChange={handleChange}
-//         />       
-//         <input
-//           type="text"
-//           placeholder="Gender..."
-//           name = "gender"
-//           value = {registerData.gender}
-//           onChange={handleChange}
-//         />       
-//         <input
-//           type="text"
-//           placeholder="Username..."
-//           name = "username"
-//           value = {registerData.username}
-//           onChange={handleChange}
-//         />       
-//         <input
-//           type="text"
-//           placeholder="Email..." 
-//           name = "email"
-//           value = {registerData.email}
-//           onChange={handleChange}
-//         />        
-//          <input
-//           type="text"
-//           placeholder="Password..." 
-//           name = "password"
-//           value = {registerData.password}
-//           onChange={handleChange}
-//         />
-//         <input
-//           type="text"
-//           placeholder="Repeat Password..." 
-//           name ="repeatPassword"
-//           value = {registerData.repeatPassword}
-//           onChange={handleChange}
-//         />
-//         <input
-//           type="checkbox" 
-//           onChange={handleChange}
-//         />
-//       </form>
-//       <button onClick={() => navigate("/dashboard")}>Sign Me Up!</button>
-//       <p>Forgot Password</p>
-//     </div>
-//   )
-// }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  return (
+    <div className="register">
+      <div className="imaged">
+        <img src={write} />
+      </div>
+      <form className="registerForm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Fullname..."
+          name = "fullname"
+          value = {registerData.fullname}
+          onChange={handleChange}
+        />       
+        <input
+          type="text"
+          placeholder="Gender..."
+          name = "gender"
+          value = {registerData.gender}
+          onChange={handleChange}
+        />       
+        <input
+          type="text"
+          placeholder="Username..."
+          name = "username"
+          value = {registerData.username}
+          onChange={handleChange}
+        />       
+        <input
+          type="text"
+          placeholder="Email..." 
+          name = "email"
+          value = {registerData.email}
+          onChange={handleChange}
+        />        
+         <input
+          type="text"
+          placeholder="Password..." 
+          name = "password"
+          value = {registerData.password}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Repeat Password..." 
+          name ="repeatPassword"
+          value = {registerData.repeatPassword}
+          onChange={handleChange}
+        />
+        <input
+          type="checkbox" 
+          onChange={handleChange}
+        />
+      </form>
+      <button onClick={() => navigate("/dashboard")}>Sign Me Up!</button>
+      <p>Forgot Password</p>
+    </div>
+  )
+}
 
-// export default Register
+export default Register
