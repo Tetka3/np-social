37e4874bb591c96b377d863042c30bfa227e9d2c
Titus Njema <njema3@gmail.com>
2024-10-24 13:43:14 +0300

    first commit

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+// import { Outlet } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+
+
+const MainLayout = () => {
+  return (
+    <div>
+        <Dashboard/>             
+    </div>
+  )
+}
+
+export default MainLayout
