23b0c97fce32a70a2d2afd5a29778e07ee0259f2
Titus Njema <njema3@gmail.com>
2024-10-24 08:22:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import MainLayout from "./layouts/MainLayout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route index element={<HomePage />} /> 
+      <Route path="/login" element={<Login />} />       
+      <Route path="/register" element={<Register />} /> 
+      <Route path="/dashboard" element={<MainLayout/>} >            
+        <Route path="/dashboard" element={<Dashboard />} />  
+      </Route>
+    </>
+  )
+);
+
+function App() {  
+  
+
+  return (
+    <>
+     <RouterProvider router={router}/>
+      
+    </>
+  )
+}
+
+export default App
+
+
+
