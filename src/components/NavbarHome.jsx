07dfe257fdb338399bf563425f823facfc6c181d
Titus Njema <njema3@gmail.com>
2024-10-24 14:17:32 +0300

    first commit

---
+++ src/components/NavbarHome.jsx
@@ -0,0 +1,30 @@
+import social from "../assets/social.jpg";
+import "./style.css";
+import { useNavigate } from 'react-router-dom';
+
+
+const NavbarHome = () => {
+    
+    const navigate = useNavigate();
+
+  return (
+    <div className='navbar'>
+        <div className="leftnav">
+            <div className="leftDiv">
+                <img src={social} alt="logo" onClick={() => navigate("/")}/>
+            </div>
+            <div className="rightDiv">
+                <p>FOLLOW ME I FOLLOW YOU</p>
+                <span>Your No. 1 Social Network Site</span>
+            </div>   
+        </div>
+        <div className="centernav"></div>
+        <div className="rightnav">
+            <button onClick={() => navigate("/login")}>Log In</button>                        
+            <button onClick={() => navigate("/register")}>Register</button>    
+        </div>        
+    </div>
+  )
+}
+
+export default NavbarHome
