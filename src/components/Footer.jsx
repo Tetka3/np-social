20e2025f286b7b0a2048fc734b40c2b05a37f401
Titus Njema <njema3@gmail.com>
2024-10-24 14:18:32 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="top-footer">
+        <p><b>FOLLOW ME I FOLLOW YOU</b> is neither affiliated with nor endorsed by Reddit, YouTube, Instagram, Diigo, Delicious,  Tumblr, Linkedin, VK, Twitter, Facebook, Pinterest, or any other social network or Bookmarking site</p>
+      </div>
+      <div className="bottom-footer">
+        <ul>
+          <li>FAQ</li>
+          <li>Privacy</li>
+          <li>Terms</li>
+          <li>Contact</li>
+          <li>Advertise</li>
+        </ul> 
+        <p>Copyright Titus Njema &copy; 2024</p> 
+      </div> 
+    </div> 
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import social from "../assets/social.jpg";
+import "./style.css";
+
+
+const Navbar = () => {
+    
+    const navigate = useNavigate();
+  return (
+    <div className='navbar'>
+        <div className="leftnav">
+            <div className="leftDiv">
+                <img src={social} alt="logo" onClick={() => navigate("/")}/>
+            </div>
+            <div className="rightDiv">
+                <p>FOLLOW ME I FOLLOW YOU</p>
+                <span>Your No. 1 Social Network Site!</span>
+            </div>   
+        </div>
+        <div className="centernav"></div>
+        <div className="rightnav">            
+            <button>Coins: </button>
+            <button>Member</button>
+            <button onClick={() => navigate("/")}>Logout</button>              
+        </div>        
+    </div>
+  )
+}
+
+export default Navbar
