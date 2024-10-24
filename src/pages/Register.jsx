95912544a879efd11538d9a8489b61992028d089
Titus Njema <njema3@gmail.com>
2024-10-24 14:45:10 +0300

    first commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -34,42 +34,42 @@ const Register = () => {
           type="text"
           placeholder="Fullname..."
           name = "fullname"
-          value = {registerData.fullname}
+          // value = {registerData.fullname}
           // onChange={handleChange}
         />       
         <input
           type="text"
           placeholder="Gender..."
           name = "gender"
-          value = {registerData.gender}
+          // value = {registerData.gender}
           // onChange={handleChange}
         />       
         <input
           type="text"
           placeholder="Username..."
           name = "username"
-          value = {registerData.username}
+          // value = {registerData.username}
           // onChange={handleChange}
         />       
         <input
           type="text"
           placeholder="Email..." 
           name = "email"
-          value = {registerData.email}
+          // value = {registerData.email}
           // onChange={handleChange}
         />        
          <input
           type="text"
           placeholder="Password..." 
           name = "password"
-          value = {registerData.password}
+          // value = {registerData.password}
           // onChange={handleChange}
         />
         <input
           type="text"
           placeholder="Repeat Password..." 
           name ="repeatPassword"
-          value = {registerData.repeatPassword}
+          // value = {registerData.repeatPassword}
           // onChange={handleChange}
         />
         <input
