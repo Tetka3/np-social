1bd21fcdc6d8ceb9448fb05c053578bc91bcb76f
Titus Njema <njema3@gmail.com>
2024-10-24 13:33:29 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,36 @@
-// import {
-//   createBrowserRouter,
-//   createRoutesFromElements,
-//   Route,
-//   RouterProvider,
-// } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 // import Dashboard from "./pages/Dashboard";
 // import MainLayout from "./layouts/MainLayout";
-// import Login from "./pages/Login";
+
 // import Register from "./pages/Register";
 
 
-// const router = createBrowserRouter(
-  // createRoutesFromElements(
-    // <>
-      // <Route index element={<HomePage />} /> 
-      {/* <Route path="/login" element={<Login />} />        */}
-      {/* <Route path="/register" element={<Register />} />  */}
-      {/* <Route path="/dashboard" element={<MainLayout/>} >             */}
-        {/* <Route path="/dashboard" element={<Dashboard />} />   */}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route index element={<HomePage />} />   
+          
       
-    // </>
-  // )
-// );
+    </>
+  )
+);
 
-function App() {  
-  
+function App() {    
 
   return (
-    <>
-     {/* <RouterProvider router={router}/> */}
-      <HomePage/>
+    <>   
+      <RouterProvider router={router}/>       
     </>
   )
 }
 
-export default App
+export default App;
 
 
 
