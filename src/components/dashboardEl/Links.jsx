06dce38d21fa074b9043c56a1f1e63bf89e24e8e
Titus Njema <njema3@gmail.com>
2024-10-25 09:14:45 +0300

    first commit

--- src/components/dashboardEl/Links.jsx
+++ src/components/dashboardEl/Links.jsx
@@ -6,17 +6,17 @@ const Links = () => {
   return (
     <div className='links'>
         <div className="left">            
-            <Link to="/dashboard"><li className='activeLink'>Home</li></Link>
-            <Link to="/dashboard"><li>Add New</li></Link>
-            <Link to="/dashboard"><li>Earn Coins</li></Link>
-            <Link to="/dashboard"><li>Buy Coins</li></Link>            
+            <Link to="/np-social/dashboard"><li className='activeLink'>Home</li></Link>
+            <Link to="/np-social/dashboard"><li>Add New</li></Link>
+            <Link to="/np-social/dashboard"><li>Earn Coins</li></Link>
+            <Link to="/np-social/dashboard"><li>Buy Coins</li></Link>            
         </div>
         <div className="center"></div>
         <div className="right">
             <ul>
-                <Link to="/dashboard"><li className='activeLink'>Extras</li></Link>
-                <Link to="/dashboard"><li>Support</li></Link>
-                <Link to="/dashboard"><li>Account</li></Link>
+                <Link to="/np-social/dashboard"><li className='activeLink'>Extras</li></Link>
+                <Link to="/np-social/dashboard"><li>Support</li></Link>
+                <Link to="/np-social/dashboard"><li>Account</li></Link>
             </ul>
         </div>
       
