e4d5ae570c001026b2d267b80fec28728fabbbee
Titus Njema <njema3@gmail.com>
2024-10-31 15:26:24 +0300

    first commit

--- src/components/mainEl/Booster.jsx
+++ src/components/mainEl/Booster.jsx
@@ -16,8 +16,7 @@ const Booster = () => {
   return (
     <div className='booster'>
       <div className="info">
-          <h3>Social Media & Traffic Booster</h3>
-          <h3>Social Media & Traffic Booster</h3>
+          <h3>Social Media & Traffic Booster</h3>          
           <p>Whether you're a new business or an old one, you can count on our platforms to boost traffic. It does not matter. All that matters is your committment and desire toexcel by connecting to potential customers and business partners. You can improve your social network with the following social media platforms.</p><br></br>
       </div>
       <div className="socials">
