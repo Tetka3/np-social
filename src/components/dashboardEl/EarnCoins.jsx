0a4939196cf695a78de18c903d2e142eb391c31d
Titus Njema <njema3@gmail.com>
2024-11-06 19:31:11 +0300

    first commit

--- src/components/dashboardEl/EarnCoins.jsx
+++ src/components/dashboardEl/EarnCoins.jsx
@@ -1,10 +1,12 @@
 import "./links.css";
+import arrow from '../../assets/arrow.png';
 
 const EarnCoins = ({ onAdd }) => {  
 
   return (
-    <div className='earnCoins'>
-      <button onClick={onAdd}>Earn Coins</button>
+    <div className='earnCoins' onClick={onAdd}>
+      <p>Earn Coins</p>
+      <img src={arrow} className="arrow"/>
     </div>
   )
 }
