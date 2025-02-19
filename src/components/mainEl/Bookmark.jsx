8f6e385a28379798dc84f78c7d0bcd86b6d61b32
Titus Njema <njema3@gmail.com>
2025-02-19 18:02:00 +0300

    first commit

--- src/components/mainEl/Bookmark.jsx
+++ src/components/mainEl/Bookmark.jsx
@@ -1,12 +1,15 @@
 import "./business.css";
 import { Link } from "react-router-dom";
+import SignUp from "./SignUp";
 
 const Bookmark = () => {
   return (
     <div className='bookmark'>
       <h3>Likes, Shares & Bookmarks</h3><br></br> 
       <p>Why do you need likes and shares? Social media currently plays a major role in boosting your SEO. When you increase your social network, yousimply increase your social presence. You can rely on us for likes and shares. </p><br></br>
-      <Link to="/np-social/register"><button>Sign Up Free</button></Link>
+      <Link to="/np-social/register">
+        <SignUp />        
+      </Link>
     </div>
   )
 }
