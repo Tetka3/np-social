1f497fcd852cc084bca2712cc442b84290459134
Titus Njema <njema3@gmail.com>
2025-02-19 17:54:14 +0300

    first commit

--- src/components/mainEl/Website.jsx
+++ src/components/mainEl/Website.jsx
@@ -1,6 +1,7 @@
 import website from "../../assets/website.png";
 import { Link } from "react-router-dom";
 import './hero.css'
+import SignUp from "./SignUp";
 
 const Website = () => {
   return (
@@ -11,7 +12,9 @@ const Website = () => {
       <div className="texted">
         <h3>Your Website</h3>
         <p>Promote your website accross all major networks and explode your traffic & sales whilst improving SEO</p>
-        <Link to="/np-social/register"><button>Sign Up Free</button></Link>
+        <Link to="/np-social/register">
+          <SignUp />        
+        </Link>
       </div>
        
     </div>
