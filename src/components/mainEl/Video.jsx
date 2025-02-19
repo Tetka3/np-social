5ded08ad7e4914bc42feed7c77be5890c3be00e7
Titus Njema <njema3@gmail.com>
2025-02-19 17:55:54 +0300

    first commit

--- src/components/mainEl/Video.jsx
+++ src/components/mainEl/Video.jsx
@@ -1,6 +1,7 @@
 import "./business.css";
 import video from "../../assets/video.png";
 import { Link } from "react-router-dom";
+import SignUp from "./SignUp";
 
 const Video = () => {
   return (
@@ -11,7 +12,9 @@ const Video = () => {
       <div className="texted">
         <h3>Videos & Tracks</h3>
         <p>Get your videos and tracks to the right people to improve your exposure to different groups of people</p>
-        <Link to="/np-social/register"><button>Sign Up Free</button></Link>
+        <Link to="/np-social/register">
+          <SignUp />        
+        </Link>
       </div>
       
     </div>
