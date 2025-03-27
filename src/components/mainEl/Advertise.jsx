d57e8c46ae74b1ec4ccd7c627d09b372dd416ec3
Titus Njema <njema3@gmail.com>
2025-03-27 09:06:27 +0300

    first commit

--- src/components/mainEl/Advertise.jsx
+++ src/components/mainEl/Advertise.jsx
@@ -4,7 +4,7 @@ const Advertise = () => {
   return (
     <div className='adverts'>
       <p>Advertise Here</p>
-      <p>From $20 per 25,000 impressions</p>
+      <p>From $20 per 25,000 Impressions</p>
     </div>
   )
 }
