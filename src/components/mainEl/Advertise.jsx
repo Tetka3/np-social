38948eb357b0b3ab405a133ca608db6317479017
Titus Njema <njema3@gmail.com>
2025-03-28 19:36:25 +0300

    first commit

--- src/components/mainEl/Advertise.jsx
+++ src/components/mainEl/Advertise.jsx
@@ -2,7 +2,7 @@ import "./hero.css";
 
 const Advertise = () => {
   return (
-    <div className='adverts'>
+    <div className='advertisement'>
       <p>Advertise Here</p>
       <p>From $20 per 25,000 Impressions</p>
     </div>
