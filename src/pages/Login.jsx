b33205b620f0044ee0396a45ee31488f474591cb
Titus Njema <njema3@gmail.com>
2025-04-07 12:53:57 +0300

    first commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 // import { useState } from "react";
 import "./home.css";
 import { Link, useNavigate } from 'react-router-dom';
-import twitter from "../../assets/twitter.jpg";
+import login from "../../assets/login.jpeg";
 
 const Login = () => {
 
