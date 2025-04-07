5944dbce1f7de612d2476c0b630a143df33462cc
Titus Njema <njema3@gmail.com>
2025-04-07 13:02:44 +0300

    first commit

--- src/components/NavbarHome.jsx
+++ src/components/NavbarHome.jsx
@@ -1,5 +1,5 @@
 import social from "../assets/social.jpg";
-import login from "../../assets/login.jpeg"; 
+import login from "../assets/login.jpeg"; 
 import "./style.css";
 import { useNavigate } from 'react-router-dom';
 
